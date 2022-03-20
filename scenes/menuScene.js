0e63bb7fe4743466cb36af65ca6269415246d511
Tim Stumper <tjstumpy@udel.edu>
2022-03-20 15:18:34 -0400

    added esc key listener for menu navigation

--- scenes/menuScene.js
+++ scenes/menuScene.js
@@ -5,5 +5,15 @@ class menuScene extends Phaser.Scene{
 
     create(){
         this.add.text(20, 20, "this is the menu");
+
+        //escape keyboard input
+        this.escKey = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ESC);
+
     }
+
+    update(){
+        if(Phaser.Input.Keyboard.JustUp(this.escKey)){
+            this.scene.start("mainMenu");
+        }
+    }  
 }
