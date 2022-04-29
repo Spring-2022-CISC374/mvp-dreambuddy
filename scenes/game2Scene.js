7fdb35b1ff0293c5df22b13c2bcd911725f3ee27
Emily Taylor <eetaylor@udel.edu>
2022-04-29 16:54:45 -0400

    Loaded all of the card images

--- scenes/game2Scene.js
+++ scenes/game2Scene.js
@@ -11,9 +11,32 @@ class game2Scene extends Phaser.Scene{
 
     preload() {
         this.load.image('cardBack', 'assets/images/cardBack.png');
+
+        // Load key cards
+        this.load.image("kingCKey", "assets/images/card-images/cards-key-img/king-cloves-key.png");
+        this.load.image("kingDKey", "assets/images/card-images/cards-key-img/king-diamonds-key.png");
+        this.load.image("kingHKey", "assets/images/card-images/cards-key-img/king-hearts-key.png");
+        this.load.image("kingSKey", "assets/images/card-images/cards-key-img/king-spades-key.png");
+        this.load.image("queenCKey", "assets/images/card-images/cards-key-img/queen-cloves-key.png");
+        this.load.image("queenDKey", "assets/images/card-images/cards-key-img/queen-diamonds-key.png");
+        this.load.image("queenHKey", "assets/images/card-images/cards-key-img/queen-hearts-key.png");
+        this.load.image("queenSKey", "assets/images/card-images/cards-key-img/queen-spades-key.png");
+
+        // Load definition cards
+        this.load.image("kingCDef", "assets/images/card-images/cards-def-img/king-cloves-def.png");
+        this.load.image("kingDDef", "assets/images/card-images/cards-def-img/king-diamonds-def.png");
+        this.load.image("kingHDef", "assets/images/card-images/cards-def-img/king-hearts-def.png");
+        this.load.image("kingSDef", "assets/images/card-images/cards-def-img/king-spades-def.png");
+        this.load.image("queenCDef", "assets/images/card-images/cards-def-img/queen-cloves-def.png");
+        this.load.image("queenDDef", "assets/images/card-images/cards-def-img/queen-diamonds-def.png");
+        this.load.image("queenHDef", "assets/images/card-images/cards-def-img/queen-hearts-def.png");
+        this.load.image("queenSDef", "assets/images/card-images/cards-def-img/queen-spades-def.png");
+        
     }
 
     create(){
+        //this.cardFronts = this.physics.add.group();
+
 		this.boardArray = [];
         for (let row = 0; row < 4; row++) {
 			this.boardArray[row] = [];
